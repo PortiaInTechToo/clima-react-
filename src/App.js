3f9263ccdea52c83d8178903a9439dfd558ad985
[Portia Mthethwa] <[portiadigital7@gmail.com]>
2024-11-27 02:39:20 +0200

    Added the footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Portia Mthethwa and is open-sourced on {""}
+        <a
+          href="https://github.com/PortiaInTechToo/clima-react-"
+          target="_blank"
+        >
+          Github
+        </a>
+        {""}and is hosted on{""}
+        <a href="https://silver-choux-e22fb1.netlify.app/" target="_blank">
+          Netlify.
+        </a>
+      </footer>
+    </div>
+  );
 }
