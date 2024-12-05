b87ee1880c9b24acd7265d795c2914d798453e86
[Portia Mthethwa] <[portiadigital7@gmail.com]>
2024-12-05 02:52:46 +0200

    added an animated weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       city: response.data.city,
       description: response.data.condition.description,
-      iconUrl: response.data.condition.icon_url,
+      icon: response.data.weather[0].icon,
       date: new Date(
         response.data.time ? response.data.time * 1000 : Date.now()
       ),
